5a3e191395596c2535a998eebf17027a29005b21
sbx_user1051 <sbx_user1051@169.254.19.229>
2023-06-03 23:02:33 +0000

    Sat Jun 03 23:02:33 UTC 2023

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -4,7 +4,8 @@
 # friend_name = unicode string
 def hello(friend_name):
     if friend_name:
-        return "Hello, World!"
+        return f"Hello, {friend_name}!"
     return ""
     # raise NotImplementedError()
+
 
