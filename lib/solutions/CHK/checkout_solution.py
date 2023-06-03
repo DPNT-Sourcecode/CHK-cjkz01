922de9706859984e90a21ee90b7d9ea50d1f135f
sbx_user1051 <sbx_user1051@169.254.19.229>
2023-06-03 23:50:40 +0000

    Sat Jun 03 23:50:40 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,26 +6,29 @@ from collections import Counter
 def checkout(skus):
     # raise NotImplementedError()
     product = {
-        'A': {-1: 50, 3:130},
-        'B': {-1: 30, 2:45},
-        'C': {-1: 20},
-        'D': {-1: 15},
+        'A': {'1': 50, 'bonus':3,'amount': 130},
+        'B': {'1': 30, 'bonus':2, 'amount': 45},
+        'C': {'1': 20},
+        'D': {'1': 15},
     }
 
     total = 0
     counters = Counter(skus)
     for key, val in counters.items():
-        if key.upper() in product:
-            if val in product[key.upper()]:
-                total += product[key.upper()][val]
+        if key in product:
+            if val >= product['bonus']:
+                div = int(val / product['bonus'])
+                rem = val % product['bonus']
+                total += (product[key]['amount'] * div) + (product[key]['1'] * rem)
 
             else:
-                total += val * product[key.upper()][-1]
+                total += val * product[key]['1']
 
         else:
             return -1
 
     return total
+
 
 
 
