53708ebbeb42250e17ff6122cb4226e51d580a20
sbx_user1051 <sbx_user1051@169.254.19.229>
2023-06-03 23:29:24 +0000

    Sat Jun 03 23:29:24 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,33 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+from collections import Counter
 def checkout(skus):
-    raise NotImplementedError()
+    # raise NotImplementedError()
+    product = {
+        'A': {-1: 50, 3:130},
+        'B': {-1: 30, 2:45},
+        'C': {-1: 20},
+        'B': {-1: 15},
+
+
+    }
+
+    total = 0
+    counters = Counter(skus)
+    for key, val in counters.items():
+        if key.upper() in product:
+            if val in product[key.upper()]:
+                total += product[key.upper()]
+
+            else:
+                total += val * product[key.upper()][-1]
+
+        else:
+            return -1
+
+    return total
+
+
+
+
---
+++ test/solution_tests/CHK/test_sum.py
@@ -0,0 +1,12 @@
+from solutions.SUM import sum_solution
+
+
+class TestSum():
+    def test_sum(self):
+        assert sum_solution.compute(1, 2) == 3
+
+    def test_two(self):
+        assert sum_solution.compute(2, 3) == 5
+
+    def test_three(self):
+        assert sum_solution.compute(2, 3) > 3
