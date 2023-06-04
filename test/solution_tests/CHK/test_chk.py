3bbed1752914fb96a6c372ab0046450b769be6e2
sbx_user1051 <sbx_user1051@169.254.19.229>
2023-06-04 00:58:43 +0000

    Sun Jun 04 00:58:43 UTC 2023

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -4,12 +4,13 @@ from solutions.CHK import checkout_solution
 class TestSum():
     def test_checkout(self):
         assert checkout_solution.checkout("EE") == 80
-        assert checkout_solution.checkout("EE") == 80
-        assert checkout_solution.checkout("EE") == 80
+        assert checkout_solution.checkout("ABCDE") == 155
+        assert checkout_solution.checkout("E") == 40
 
     # def test_two(self):
     #     assert sum_solution.compute(2, 3) == 5
     #
     # def test_three(self):
     #     assert sum_solution.compute(2, 3) > 3
+
 
