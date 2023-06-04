6e6b69e45a54bf6ea813534a648f290e0fa43818
sbx_user1051 <sbx_user1051@169.254.19.229>
2023-06-04 02:23:04 +0000

    Sun Jun 04 02:23:04 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -15,6 +15,7 @@ def checkout(skus):
         'C': {-1: 20},
         'D': {-1: 15},
         'E': {-1: 40, 2:'B', 'free_bonus': [2]},
+        'F': {-1: 10, 2:'F', 'free_bonus': [2]},
     }
 
 
@@ -27,10 +28,11 @@ def checkout(skus):
             return -1
 
         if 'free_bonus' in product[key]:
+            if key == 'F' and val != 3:
+                continue
             for bonus in product[key]['free_bonus']:
                 div = int(val / bonus)
                 if product[key][bonus] in counters:
-                    # total -= product[product[key][bonus]].get( div , product[product[key][bonus]][-1] * div)
                     counters[product[key][bonus]] -= min(div, counters[product[key][bonus]])
 
     for key, val in counters.items():
@@ -46,17 +48,8 @@ def checkout(skus):
         else:
             total += val * product[key][-1]
 
-        # if 'free_bonus' in product[key]:
-        #     for bonus in product[key]['free_bonus']:
-        #         div = int(val / bonus)
-        #         if product[key][bonus] in counters:
-        #             total -= product[product[key][bonus]].get( div , product[product[key][bonus]][-1] * div)
-
-
-
 
     return total
-
 
 
 
