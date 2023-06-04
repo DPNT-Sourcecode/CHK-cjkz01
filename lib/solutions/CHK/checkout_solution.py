bc6ef6cd39b0122728383ccaaeb8fedf12c2ce36
sbx_user1051 <sbx_user1051@169.254.19.229>
2023-06-04 00:29:54 +0000

    Sun Jun 04 00:29:54 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,28 +6,39 @@ from collections import Counter
 def checkout(skus):
     # raise NotImplementedError()
     product = {
-        'A': {'1': 50, 'bonus':3,'amount': 130},
-        'B': {'1': 30, 'bonus':2, 'amount': 45},
-        'C': {'1': 20},
-        'D': {'1': 15},
+        'A': {-1: 50, 3:130,5: 200, 'bonus': [5, 3]},
+        'B': {-1: 30, 2:45},
+        'C': {-1: 20},
+        'D': {-1: 15},
+        'E': {-1: 15, 2:'B'},
     }
 
     total = 0
     counters = Counter(skus)
     for key, val in counters.items():
         if key in product:
-            if 'bonus' in product[key] and val >= product[key]['bonus']:
-                div = int(val / product[key]['bonus'])
-                rem = val % product[key]['bonus']
-                total += (product[key]['amount'] * div) + (product[key]['1'] * rem)
+
+            if 'bonus' in product[key]:
+                rem = 0
+                for bonus in product[key]['bonus']:
+                    if product[key][bonus] in product[key]:
+                        total += product[ product[key][bonus] ][-1]
+                    else:
+
+                        div = int(val / bonus)
+                        total += (product[key][bonus] * div)
+                        rem = val % bonus
+                        val = rem
+                total += (product[key][-1] * rem)
 
             else:
-                total += val * product[key]['1']
+                total += val * product[key][-1]
 
         else:
             return -1
 
     return total
+
 
 
 
